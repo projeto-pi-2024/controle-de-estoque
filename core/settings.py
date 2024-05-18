9d70eb2055d6b22c52a49fca2dd8289a7a11349c
ricardocanedo <ricardocanedo@live.com>
2024-05-18 08:02:03 -0300

    fix static

--- core/settings.py
+++ core/settings.py
@@ -116,10 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     BASE_DIR / 'base_static',
 )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "accounts/login"
